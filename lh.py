5a3e7970987196b5cccab0fd9089ca016020a677
veritas919 <olackner@smu.edu>
2021-11-03 00:01:05 -0500

    case 3 done. check for case 2, if key without numbers has 1 page or 0 for calculating capacity

--- lh.py
+++ lh.py
@@ -28,6 +28,10 @@ class LinearHashing:
             self.case_1_insert(number) 
         elif self.policy == 2:
             self.case_2_insert(number) 
+        elif self.policy == 3:
+            self.case_3_insert(number)
+        else:
+            print("invalid value for policy.")
 
     ######################################################################### CASE 0 INSERT ##########################################################################
     def case_0_insert(self, num):
@@ -349,9 +353,112 @@ class LinearHashing:
 
         return split_occured 
 
-    
 
-###################### END CASE 2 #################
+############################################################################### CASE 3 ##############################################################
+
+    def case_3_insert(self, num):
+        split_occured = False
+        print("in case 3")
+        
+        # for level 0 
+        if self.level == 0:
+            # Insert number 
+            self.hash_table[0].append(num) # add number 
+            # if there is an overflow for bucket that would be split  ----- > create new bucket, rehash, level up, reset ptr 
+            if self.isOverflowedRightNow(self.ptr) == True:  
+                self.hash_table[1] = [] # create new bucket 
+
+                copy_of_bucket_0 = copy.deepcopy(self.hash_table[0])
+                bucket_0 = []
+                bucket_1 = []
+                for item in copy_of_bucket_0:
+                    # print(item)
+                    if item % 2 == 0:
+                        bucket_0.append(item)
+                    else:
+                        bucket_1.append(item) 
+                self.hash_table[0] = bucket_0
+                self.hash_table[1] = bucket_1 
+                self.level = 1
+                self.ptr = 0 
+                self.num_buckets += 1
+                split_occured = True 
+
+                # self.isOverflowedRightNow()   # MUST RECHECK    WORKS FINE WITHOUT @@@@@@@@@@@@ TAKE OUT TEMP 
+
+        # for other levels
+        else: 
+            print("else", num)
+            print("Level is: ", self.level)
+            
+            ############ INSERT NUMBER INTO BUCKET ############
+            num_as_bin = bin(num)[2:]
+            
+            bucket_key = None 
+            if len(num_as_bin) <= self.level:
+                ht_index_try_1 = num
+                if ht_index_try_1 in self.hash_table:
+                    self.hash_table[ht_index_try_1].append(num)
+                    bucket_key = ht_index_try_1
+                else:
+                    print("cant find matching bucket")
+
+            # number as binary is necessarily >= 2 
+            else: 
+                bigger_last_bits = num_as_bin[-1: -(self.level + 2) : -1][::-1]
+                smaller_last_bits = num_as_bin[-1: -(self.level + 1) : -1][::-1]
+                if int(bigger_last_bits, 2) in self.hash_table:
+                    self.hash_table[int(bigger_last_bits,2)].append(num)
+                    bucket_key = int(bigger_last_bits,2)
+                elif int(smaller_last_bits, 2) in self.hash_table:
+                    self.hash_table[int(smaller_last_bits,2)].append(num) 
+                    bucket_key = int(smaller_last_bits,2)
+                else:
+                    print("problem. number is....:", num, bigger_last_bits, smaller_last_bits)
+
+            # if there is an overflow for bucket that would be split ----- > create new bucket, rehash, move ptr. Check if leveling up, and if so, reset ptr
+            if self.isOverflowedRightNow(self.ptr) == True:
+                # print("spliiting bucket: ", )
+                bin_of_ptr =  bin(self.ptr)[2:]
+                print("spliting bucket: ", self.ptr)
+                print("\n")
+                new_bucket_value_0 = self.ptr
+                new_bucket_value_1 = self.ptr + 2**(self.level)
+
+                # rehash 
+                copy_of_bucket_being_split = copy.deepcopy(self.hash_table[self.ptr])
+                bucket_0 = []
+                bucket_1 = []
+                for item in copy_of_bucket_being_split:
+                    # print("item in splitting bucket...",item)
+                    item_as_bin = bin(item)[2:]
+
+                    if len(item_as_bin) <= self.level + 1:
+                        k_bits_as_int = item 
+                    else:
+                        k_bits_as_int = int(item_as_bin[-1: -(self.level + 2) : -1][::-1], 2)
+                    
+                    if k_bits_as_int == new_bucket_value_0:
+                        bucket_0.append(item)
+                    elif k_bits_as_int == new_bucket_value_1:
+                        bucket_1.append(item)
+                    else:
+                        print("problemo. item val is: ", item) 
+
+                self.hash_table[new_bucket_value_0] = bucket_0
+                self.hash_table[new_bucket_value_1] = bucket_1 
+                
+                self.num_buckets += 1
+                self.ptr +=1 
+                split_occured = True 
+                # post split, check if next level now
+                if self.num_buckets == 2**(self.level+1):
+                    print("incrementing level")
+                    print(self.num_buckets) 
+                    self.level += 1
+                    self.ptr = 0
+ 
+        return split_occured 
 
     # use for Case 2 
     def get_current_capacity_ratio(self):
@@ -424,7 +531,8 @@ class LinearHashing:
 if __name__ == "__main__":
     # x = LinearHashing(page_size = 2, policy = 0, max_overflow = 2)
     # x = LinearHashing(page_size = 2, policy = 1, max_overflow = 0) # should function same as default case 
-    x = LinearHashing(page_size = 2, policy = 2, size_limit = 0.7)
+    # x = LinearHashing(page_size = 2, policy = 2, size_limit = 0.7)
+    x = LinearHashing(page_size = 2, policy = 3)
 
     x.insert(2)
     x.insert(0)
