f0779bc3a230c1461d2812b3a0cf9c1a1355183e
veritas919 <olackner@smu.edu>
2021-12-27 16:37:55 -0800

    runner_1 cleaned up

--- linear_hashing/examples/runner_1.py
+++ linear_hashing/examples/runner_1.py
@@ -1,11 +1,16 @@
 from lh import LinearHashing
 
 if __name__ == "__main__":
+
+    # create linear hashing object 
+    x = LinearHashing(page_size = 3, policy = 3) 
+
+    # the lines below show a few configuration options for the LinearHashing object 
     # x = LinearHashing(page_size = 5, policy = 0)
     # x = LinearHashing(page_size = 3, policy = 1, max_overflow = 5)
     # x = LinearHashing(page_size = 2, policy = 2, size_limit = 0.7)
-    x = LinearHashing(page_size = 3, policy = 3)
 
+    # insert numbers into the hash table 
     x.general_insert(2)
     x.general_insert(0)
     x.general_insert(1) 
@@ -24,19 +29,20 @@ if __name__ == "__main__":
     x.general_insert(111)
     x.general_insert(42)
 
+    # print contents of hash table as well as the level and pointer to a file 
+    x.PrintFile("./data/table_content/hash_table_1.txt")  
 
-    # x.PrintFile("output2.txt")
-    arr = x.ListBucket(1)
-    print(arr)
-
+    # search hash table for number specified and print the number of pages searched to find the specified number in the hash table 
     print(x.Search(45))
     print(x.Search(23)) 
     print(x.Search(950))
     print(x.Search(2))
     print(x.Search(99)) 
 
+    # print hash table contents to console 
     x.Print()
 
+    # print stats to console 
     stats_info = x.GetStats()
     print("~~~~~~~~~~STATS~~~~~~~~~~~~~")
 
@@ -48,57 +54,3 @@ if __name__ == "__main__":
     print("access count", stats_info.Access())
     print("access count during insert only", stats_info.AccessInsertOnly())
     print("space utilization", stats_info.SpaceUtilization())
-
-'''
-    ##################################################################### Getting skewed distribution testing data ###########################################
-
-    (nums_to_insert, nums_to_search) = get_skewed_test_sets()
-
-    case_0 = LinearHashing(page_size = 3, policy = 0)
-    case_1 = LinearHashing(page_size = 3, policy = 1, max_overflow = 5)
-    case_2 = LinearHashing(page_size = 3, policy = 2, size_limit = 0.9)
-    case_3 = LinearHashing(page_size = 3, policy = 3)
-    
-    case_0.testing("skewed_case_0.txt", nums_to_insert, nums_to_search)
-    case_1.testing("skewed_case_1.txt", nums_to_insert, nums_to_search)
-    case_2.testing("skewed_case_2.txt", nums_to_insert, nums_to_search)
-    case_3.testing("skewed_case_3.txt", nums_to_insert, nums_to_search)
-
-
-    print("done") 
-
-    
-    ###################################################################### Getting nearly uniform distribution testing data #######################################
-    (nums_to_insert, nums_to_search) = get_nearly_uniform_test_sets()
-
-    case_0 = LinearHashing(page_size = 3, policy = 0)
-    case_1 = LinearHashing(page_size = 3, policy = 1, max_overflow = 5)
-    case_2 = LinearHashing(page_size = 3, policy = 2, size_limit = 0.9)
-    case_3 = LinearHashing(page_size = 3, policy = 3)
-    
-    case_0.testing("nearly_uniform_case_0.txt", nums_to_insert, nums_to_search)
-    case_1.testing("nearly_uniform_case_1.txt", nums_to_insert, nums_to_search)
-    case_2.testing("nearly_uniform_case_2.txt", nums_to_insert, nums_to_search)
-    case_3.testing("nearly_uniform_case_3.txt", nums_to_insert, nums_to_search)
-
-
-    print("done") 
-
-    
-    #################################################################### Getting random distribution testing data ###################################################### 
-    (nums_to_insert, nums_to_search) = get_random_test_sets()
-
-    case_0 = LinearHashing(page_size = 3, policy = 0)
-    case_1 = LinearHashing(page_size = 3, policy = 1, max_overflow = 5)
-    case_2 = LinearHashing(page_size = 3, policy = 2, size_limit = 0.9)
-    case_3 = LinearHashing(page_size = 3, policy = 3)
-    
-    case_0.testing("random_case_0.txt", nums_to_insert, nums_to_search)
-    case_1.testing("random_case_1.txt", nums_to_insert, nums_to_search)
-    case_2.testing("random_case_2.txt", nums_to_insert, nums_to_search)
-    case_3.testing("random_case_3.txt", nums_to_insert, nums_to_search)
-
-
-    print("done") 
-    
-    '''
